45611cfbd37d69667a478d7201084e50d602d4a3
Liam Francisco <liamf@nevada.unr.edu>
2023-04-24 08:41:29 -0700

    added an enemy, enemy projectile, and ai

---
+++ CS 381 Final Project/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject projectilePrefab;
+
+    GameObject enemyProjectile;
+    Vector3 distance;
+    float shootTimer;
+    float bufferTime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        shootTimer = 0;
+        bufferTime = Random.Range(0.5f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = transform.position - player.transform.position;
+
+        if(distance.magnitude < 5)
+        {
+            transform.LookAt(player.transform);
+            if(shootTimer == bufferTime)
+            {
+                enemyProjectile = Instantiate(projectilePrefab, transform.position + transform.forward, transform.rotation);
+                enemyProjectile.GetComponent<Rigidbody>().AddForce(transform.forward*500);
+                bufferTime = Random.Range(3f, 5f);
+                shootTimer = 0;
+            }
+        }
+
+        shootTimer += Time.deltaTime;
+
+        if(shootTimer > bufferTime)
+        {
+            shootTimer = bufferTime;
+        }
+
+    }
+}
