c2c8e419b53be3ffb08f496ecb211a47f55a842e
markmarsala <markmarsala@nevada.unr.edu>
2023-04-26 13:00:19 -0700

    Level cleanup + UI + credits

--- CS 381 Final Project/Assets/Scripts/PlayerController.cs
+++ CS 381 Final Project/Assets/Scripts/PlayerController.cs
@@ -10,7 +10,9 @@ public class PlayerController : MonoBehaviour
     public int health;
     public float damageTimer;
     public float iTime;
-    public TextMeshProUGUI healthText;
+    //public TextMeshProUGUI healthText;
+    public GameObject heart2;
+    public GameObject heart3;
 
     // Start is called before the first frame update
     void Start()
@@ -22,9 +24,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W))
         {
-            transform.position += new Vector3(0, 0, velocity * Time.deltaTime); 
+            transform.position += new Vector3(0, 0, velocity * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.S))
@@ -42,13 +44,19 @@ public class PlayerController : MonoBehaviour
             transform.position += new Vector3(velocity * Time.deltaTime, 0, 0);
         }
 
-        if(Input.GetKeyUp(KeyCode.LeftShift))
+        if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             float gravY = Physics.gravity.y;
-            Physics.gravity =  new Vector3(1, -gravY, 1);
+            Physics.gravity = new Vector3(1, -gravY, 1);
         }
 
+<<<<<<< Updated upstream
         if(damageTimer < iTime)
+=======
+
+
+        if (damageTimer < iTime)
+>>>>>>> Stashed changes
             damageTimer += Time.deltaTime;
 
         if (damageTimer > iTime)
@@ -56,9 +64,9 @@ public class PlayerController : MonoBehaviour
 
         health = Mathf.Clamp(health, 0, 3);
 
-        healthText.text = "Health: " + health + "/3";
+        //healthText.text = "Health: " + health + "/3";
 
-        if(health <= 0)
+        if (health <= 0)
         {
             SceneManager.LoadScene("Level1");
         }
@@ -66,13 +74,26 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.CompareTag("Damaging")) 
+        if (collision.collider.CompareTag("Damaging"))
         {
-            if(damageTimer == iTime)
+            if (damageTimer == iTime)
             {
                 health--;
                 damageTimer = 0;
             }
+            if (health == 2)
+            {
+                heart3.SetActive(false);
+            }
+            if (health == 1)
+            {
+                heart2.SetActive(false);
+            }
+        }
+
+        if (collision.collider.CompareTag("Gem"))
+        {
+            SceneManager.LoadScene("Credits");
         }
     }
 }
